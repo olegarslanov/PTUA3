9578ff1a20847688e43f7b96af43829e0ecb1fad
Oleg Arslanov <olegarslanov@email.com>
2023-02-08 19:53:53 +0200

    adding lesson

---
+++ 2023_01_30/python_operators.py
@@ -0,0 +1,17 @@
+# Python operators
+
+print(5+6) #Addition
+print(5-6) #Subtraction
+print(5*6) #Multiplication
+print(5/6) #Division
+print(5%6) #Modulus
+print(5**6) #Exponentiation
+print(5//6) #Floor Division
+
+a=40 
+print(a)
+
+
+
+print("Antanui metu:", a)
+
